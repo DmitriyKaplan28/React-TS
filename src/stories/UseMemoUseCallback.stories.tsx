63874de6e9ea749815c46658b0f8ce0cf0dfff5b
DmitriyKaplan28 <dsk007@mail.ru>
2022-10-02 21:01:54 +0300

    delete: comments is useMemo useCallback stories

--- src/stories/UseMemoUseCallback.stories.tsx
+++ src/stories/UseMemoUseCallback.stories.tsx
@@ -4,7 +4,6 @@ export default {
     title: 'useMemo',
 }
 
-
 export const Example1 = () => {
 
     const [a, setA] = useState(3)
@@ -12,19 +11,6 @@ export const Example1 = () => {
 
     let resultA = 1;
     let resultB = 1;
-
-    /*   resultA = useMemo(() => {
-           let tempResultA = 1
-           for (let i = 1; i <= a; i++) {
-               let fake = 0;
-               while (fake < 1000000000) {
-                   fake++;
-                   const fakeValue = Math.random()
-               }
-               tempResultA = tempResultA * i;
-           }
-           return tempResultA;
-       }, [a])*/
 
     for (let i = 1; i <= b; i++) {
         resultB = resultB * i;
